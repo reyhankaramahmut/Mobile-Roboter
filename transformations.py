e60afb1d721370c17572cc54e9ed989fe99cd9d7
Reyhan Karamahmut <102164159+reyhankaramahmut@users.noreply.github.com>
2022-10-29 20:15:53 +0200

    feat: update functions

--- transformations.py
+++ transformations.py
@@ -1,58 +1,58 @@
-from cmath import pi
 import numpy as np
 import math as m
 
-
 def rot(theta: float):
-    return np.array([
+    ret = np.array([
         [m.cos(theta), -m.sin(theta)],
         [m.sin(theta), m.cos(theta)]
-    ])
-
+        ])
+    return ret
 
 def rotx(theta: float):
-    return np.array([
+    ret = np.array([
         [1, 0, 0],
         [0, m.cos(theta), -m.sin(theta)],
         [0, m.sin(theta), m.cos(theta)]
-    ])
+        ])
+    return ret
 
 
 def roty(theta: float):
-    return np.array([
+    ret = np.array([
         [m.cos(theta), 0, m.sin(theta)],
         [0, 1, 0],
         [-m.sin(theta), 0, m.cos(theta)]
-    ])
+        ])
+
+    return ret
 
 
 def rotz(theta: float):
-    return np.array([
+    ret = np.array([
         [m.cos(theta), -m.sin(theta), 0],
         [m.sin(theta), m.cos(theta), 0],
         [0, 0, 1]
-    ])
+        ])
+    return ret
 
 
 def rot2trans(r: np.ndarray):
-    return np.vstack((np.hstack((r, np.zeros((r.shape[0], 1))), np.hstack((np.zeros((1, r.shape[0])), [[1]])))))
-
+    if len(r) == 2:
+        return np.append(np.append(r,np.array([[0],[0]]), axis=1), np.array([[0,0,1]]), axis=0)
+    else:
+        return np.append(np.append(r,np.array([[0],[0],[0]]), axis=1), np.array([[0,0,0,1]]), axis=0)
 
 def trans(t: np.ndarray):
     if len(t) == 2:
         return np.array([
-            [1, 0, t[0]],
-            [0, 1, t[1]],
-            [0, 0, 1]
-        ])
+            [1,0,t[0]],
+            [0,1,t[1]],
+            [0,0,1]
+            ])
     else:
         return np.array([
-            [1, 0, 0, t[0]],
-            [0, 1, 0, t[1]],
-            [0, 0, 1, t[2]],
-            [0, 0, 0, 1]
+            [1,0,0,t[0]],
+            [0,1,0,t[1]],
+            [0,0,1,t[2]],
+            [0,0,0,1]
         ])
-
-
-if __name__ == "__main__":
-    print(rotz(np.pi).round(3))
